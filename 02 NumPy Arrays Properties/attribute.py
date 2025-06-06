a37a7fe5104b5a8b2d18de393d0a375e0a108bbc
Jatin Vishwakarma <vishwakarmajatin084@gmail.com>
2025-06-06 13:41:45 +0530

    inital commit

---
+++ 02 NumPy Arrays Properties/attribute.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+arr_2d = np.array([[1,2,3], [4,5,6]])
+print(arr_2d.shape)
+print(arr_2d.size)
+print(arr_2d.ndim)
+
+arr  =np.array([45.6, 63, 98.2, 63])
+print(arr.dtype) # datatype of element int, str, float
+
+print(arr.itemsize)
